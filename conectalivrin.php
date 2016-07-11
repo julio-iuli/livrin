c23efb4774b8c203ad13102efd7a9526ee47286f
julio-iuli <carmo.iuli@gmail.com>
2016-07-11 18:40:05 -0300

    commitlinux

--- conectalivrin.php
+++ conectalivrin.php
@@ -1,12 +1,13 @@
 <?php
-		$username = "aluno";
-		$password = "123";
-		try {
-			$con = new PDO('mysql:host=localhost;dbname=livrin', $username, $password);
-			$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			//echo "conexão feita <br>";
-		} catch(PDOException $e) {
-			echo 'ERROR: ' . $e->getMessage();
-		}
+	header('Content-Type: text/html; charset=utf-8');
+	$username = "aluno";
+	$password = "123";
+	try {
+		$con = new PDO('mysql:host=localhost;dbname=livrin', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "conexão feita <br>";
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
 	
 ?>
--- servidorlivrin.php
+++ servidorlivrin.php
@@ -1,18 +1,15 @@
 <?php
+
+	include_once 'livro.class.php';
 	
-	include 'conectalivrin.php';
-	//$id_livro = $_REQUEST['id_livro'];
+	$livro = new Livro(
+	$_REQUEST['titulo'], 
+	$_REQUEST['nomeautor'], 
+	$_REQUEST['nomeeditora'], 
+	$_REQUEST['anopublicacao'],
+	$_REQUEST['iddono']
+	);
 	
-	$campo = 'ds_titulo';
-	$tabela = 'tb_livro';
-	$termo1 = 'id_livro';
-	$termo2 = 2;
-	
-	$sql = 'SELECT ' . $campo . ' FROM ' . $tabela . ' WHERE ' . $termo1 . " = " . $termo2;
-	echo $sql . '<br>';
-	$res = $con->query($sql);
-	var_dump($res);
-	$row = $res->fetch();
-	var_dump($row);
+	$livro->echo_dados();
 	
 ?>
