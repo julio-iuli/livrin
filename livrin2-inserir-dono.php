d537403dcba334dd03ebf69188ceb6c57884e7b1
julio-iuli <carmo.iuli@gmail.com>
2016-07-21 19:43:22 -0300

    limpa

---
+++ livrin2-inserir-dono.php
@@ -0,0 +1,6 @@
+<?php
+	include_once 'cabeca.php';
+	include_once 'tabela.php';
+	
+	criar_tabela('dono');
+?>
--- livrin2-inserir-livro.php
+++ livrin2-inserir-livro.php
@@ -5,7 +5,6 @@
 		<link rel="stylesheet" href="css/w3.css">
 	<head>
 	
-	<script type="text/javascript" src="js/livrin.js"></script>
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/jquery-ui-1.12.0/jquery-ui.min.js"></script>	
 	<link rel="stylesheet" href="js/jquery-ui-1.12.0/jquery-ui.min.css">
