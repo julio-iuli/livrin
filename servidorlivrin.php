f232a7e250ba2fe10388f8d722cd6380cf30187e
julio-iuli <carmo.iuli@gmail.com>
2016-07-22 01:15:35 -0300

    adiante

--- servidorlivrin.php
+++ servidorlivrin.php
@@ -2,10 +2,6 @@
 
 	include_once 'livro.class.php';
 	
-	$livro = new Livro($_REQUEST['titulo'], $_REQUEST['nomeautor'],
-	$_REQUEST['nomeeditora'], $_REQUEST['anopublicacao'],
-	$_REQUEST['iddono']);
 	
-	$livro->echo_dados();
 	
 ?>
